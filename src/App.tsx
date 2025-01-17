68b4d13bd3b31ff2e1d37ce027e3f2c8253d62c7
namanh <namanh2509@gmail.com>
2025-01-17 22:17:46 +0700

    remove redundant code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,8 +6,6 @@ import { useNavigate } from "react-router";
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  
   const navigate = useNavigate();
   return (
     <>
