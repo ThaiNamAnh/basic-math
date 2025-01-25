a6d8d1bfa39bb095a1f229981089a6e7216a9270
namanh <namanh2509@gmail.com>
2025-01-25 16:51:54 +0700

    Move youtubevideo to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 import { useNavigate } from "react-router";
-
+import VideoYouTube from "./view/VideoYoutube";
 
 function App() {
   const navigate = useNavigate();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <button onClick={() => navigate('/basic-math/phep-cong')}>Phep Cong</button>
-      <button onClick={() => navigate('/basic-math/phep-nhan')}>Phep Nhan</button>
-      <button onClick={() => navigate('/basic-math/text-to-speech')}>Text To Speech</button>
-      <button onClick={() => navigate('/basic-math/video-youtube')}>Video Youtube</button>
+      <button onClick={() => navigate("/basic-math/phep-cong")}>
+        Phep Cong
+      </button>
+      <button onClick={() => navigate("/basic-math/phep-nhan")}>
+        Phep Nhan
+      </button>
+      <button onClick={() => navigate("/basic-math/text-to-speech")}>
+        Text To Speech
+      </button>
+      <div style={{ marginBottom: 50 }}></div>
+      <VideoYouTube />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
